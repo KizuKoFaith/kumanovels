c9f91c7ac8d638536a77f0342d05d9742136d92c
KizuKoFaith <kizukofaith03@gmail.com>
2026-01-09 08:40:05 +0800

    UPDATED AD

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.get("/proxy-image", async (req, res) => {
 
 app.use("/api/novels", novelRouter);
 
-app.listen(PORT, "192.168.10.108", () => {
-  console.log(`Server running on http://192.168.10.108:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
